28b09fc0f57df63166af344c255f969fbd2c4faf
Keyto <Keyto1995@outlook.com>
2018-03-16 21:32:07 +0800

    init project;

---
+++ data/data_provider.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Author: Keyto
+# Created on: 2018/3/14
+import numpy as np
+
+from config.cfg import IMAGE_HEIGHT, IMAGE_WIDTH, MAX_CAPTCHA, CHAR_SET_LEN
+from data.gen_captcha import gen_captcha_text_and_image
+from utils.utils import rgb2gray, text2vec
+
+
+def get_next_batch(batch_size=128):
+    """
+    生成一个训练batch
+    :param batch_size: batch大小
+    :return: (image[],text[])
+    """
+    batch_x = np.zeros([batch_size, IMAGE_HEIGHT * IMAGE_WIDTH])
+    batch_y = np.zeros([batch_size, MAX_CAPTCHA * CHAR_SET_LEN])
+
+    for i in range(batch_size):
+        text, image = gen_captcha_text_and_image(size=(IMAGE_WIDTH, IMAGE_HEIGHT))
+        image = rgb2gray(image)
+
+        batch_x[i, :] = image.flatten() / 255
+        batch_y[i, :] = text2vec(text)
+
+    return batch_x, batch_y
